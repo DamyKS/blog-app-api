ab09cfde7284ae088ee8ee7543846b8fbb737cef
damyks <damianojeh2@gmail.com>
2024-06-11 16:11:38 +0100

    added static files

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 set -o errexit  # exit on error
 
 pip install -r requirements.txt
-
+python manage.py collectstatic --no-inputss
 python manage.py migrate
 python manage.py createsuperuser --no-input
